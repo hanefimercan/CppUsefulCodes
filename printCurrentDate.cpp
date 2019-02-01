3ab214d7c7b2c68f362f634055867e5ffa3c819e
hanefimercan <hanefimercan@gmail.com>
2019-02-01 23:36:40 +0300

    Update printCurrentDate.cpp

--- printCurrentDate.cpp
+++ printCurrentDate.cpp
@@ -1,11 +1,17 @@
 #include <ctime>
 #include <iostream>
-
 using namespace std;
 
-int main() {
+void printcurrentDate()
+{
     time_t t = std::time(0);   // get time now
     tm* now = std::localtime(&t);
-    cout << (now->tm_year + 1900) << '-' << (now->tm_mon + 1) << '-' <<  now->tm_mday << endl;
+    cout << (now->tm_year + 1900) << '-' << (now->tm_mon + 1) << '-' <<  now->tm_mday;
+}
+
+int main() 
+{
+    printcurrentDate();
+    cout << endl;
     return 0;
 }
